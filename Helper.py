fe691bb3d1bfc2abc7b4d0023a6c7cbcdac4356e
Sherif Nasrat Mohamed Abd El Baky <sherif_nasrat@hotmail.com>
2019-08-07 17:34:32 +0200

    Updated with Udacity reviewer comments.
    Updated the Juypter notebook and Helper.py with the comments from the Udacity review.
    Added docstrings, comments, outlined the CRISP-DM process in a better way and elaborated some of the categorical variables handling.

--- Helper.py
+++ Helper.py
@@ -4,11 +4,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def read_data(path,encoding):
+    """Reads a CSV file into a Pandas DataFrame and returns the DataFrame with specified encoding.
+
+    Parameters:
+    path (string): Path to the CSV file to be read.
+    encoding (string): The type of encoding to read the file with.
+
+    Returns:
+    Pandas DataFrame containing the CSV data with the specified encoding.
+
+   """
+
     data = pd.read_csv(path,encoding=encoding)
     return data
 
 def plot_barchart_dictionary(x,y,x_rotation,y_label,title,figsize_x=10,figsize_y=10):
-    
+    """This function draws a bar chart of the given dictionary keys on the x-axis and values on the y-axis.
+
+    Parameters:
+    x (array_like): Labels to be added on the x-axis of the bar chart.
+    y (array_like): The values that will be mapped to each label on the x-axis.
+    x_rotation (int): The rotation of the labels on the x-axis.
+    y_label (string): The label that will appear on the y-axis.
+    title (string): The title of the figure.
+    figsize_x (int): The width of the window/figure, default=10.
+    figsize_y (int): The height of the window/figure, default=10.
+
+    Returns:
+    Pandas DataFrame containing the CSV data with the specified encoding.
+
+   """
     y_pos = np.arange(len(y))
 
     plt.figure(figsize=(figsize_x,figsize_y))
@@ -20,6 +45,16 @@ def plot_barchart_dictionary(x,y,x_rotation,y_label,title,figsize_x=10,figsize_y
     plt.show()
     
 def column_rename_firstvalue (df):
+    """This function takes a Pandas DataFrame and returns a list with the first valid entry in each column for renaming later.
+
+    Parameters:
+    df (Pandas DataFrame): The dataframe with the columns that you need to rename.
+
+    Returns:
+    list containing the first valid entry in each column.
+
+   """
+
     new_headers = []
     for col in df.columns:
         first_valid_indx = df[col].first_valid_index()
